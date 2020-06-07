774115375e24d710cee059d704a1efe22877853a
Saeid Dousti <dousti_s@yahoo.com>
2020-06-07 11:09:16 -0400

    initialized variables

--- main.py
+++ main.py
@@ -22,8 +22,20 @@ import time
 import tqdm
 from PIL import Image
 
-print('hello world')
+# predefined variables
+
+Image_Height , Image_width = 75 , 75
+Batch_size = 32
+num_classes = 6
+train_class_names = ['Charlock','Common Chickweed','Fat Hen','Loose Silky-bent','Scentless Mayweed','Small-flowered Cranesbill']
+feature_space_size = 1024
+
+# this is the path in my google drive
+train_path = '/content/drive/My Drive/Python 3/AI_Seedling_train/Labeled'
+model_path = '/content/drive/My Drive/Python 3/AI_Seedling_train/Model'
+
+#train_path = '/content/drive/My Drive/Python 3/AI_Seedling_train/To-be-Labeled'
 
 if __name__ == '__main__':
 
-    pass
+    print('u')
